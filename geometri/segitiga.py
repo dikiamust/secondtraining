6b93eaedfc3622c52382eac34f9f821023c501b5
dikiamust <agungmustaqimdiki@gmail.com>
2020-12-16 16:15:02 +0700

    rev 0.1

---
+++ geometri/segitiga.py
@@ -0,0 +1,6 @@
+# RUMUS LUAS SEGITGA
+def segitiga(a, t):
+    luas_segitiga = a * t / 2
+    return luas_segitiga
+print(segitiga(20, 10))
+print(f' segitiga dengan alas 10cm dan tinggi 6cm, maka luasnya = {segitiga(20, 6)} cm^2')
